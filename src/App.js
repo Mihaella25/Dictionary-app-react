d2ed873fff1fab4688f2a04cc7cdc76699c91155
Mihaella25 <134629851+Mihaella25@users.noreply.github.com>
2023-08-29 11:08:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AddWordComponent from './AddWordComponent';
+import SearchWordComponent from './SearchWordComponent';
 
 function App() {
+  const [dictionary, setDictionary] = useState({});
+
+  const handleAddWord = (newWord) => {
+    setDictionary((prevDictionary) => ({
+      ...prevDictionary,
+      [newWord.toLowerCase()]: true,
+    }));
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1 className="heading">Dictionary App</h1>
+        </header>
+        <main>
+          <div className="section">
+            <h2>Add a Word</h2>
+            <AddWordComponent onAddWord={handleAddWord} />
+          </div>
+          <div className="section">
+            <h2>Search for a Word</h2>
+            <SearchWordComponent dictionary={dictionary} />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
